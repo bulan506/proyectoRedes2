9b882e337fd6dc6d2e7859f9f573f3159d0bbb7b
Brandon <brandonvargass12345@gmail.com>
2024-09-15 01:59:26 -0600

    comienza partida, busca el lider, avisa enemigos, el lider propone grupo y envia grupo

--- uiredes/app/GameScreen/GameScreen.tsx
+++ uiredes/app/GameScreen/GameScreen.tsx
@@ -7,23 +7,23 @@ import ModalComponent from '@/app/components/ModalComponet';
 const SERVER = process.env.NEXT_PUBLIC_SERVER;
 
 const GameScreen = ({ game, password, playerName }: any) => {
-  const [playerStatus, setPlayerStatus] = useState('');
   const [error, setError] = useState('');
   const [players, setPlayers] = useState(game.players || []);
+  const [selectedGroup, setSelectedGroup] = useState([]);
   const [showModal, setShowModal] = useState(false);
   const [modalMessage, setModalMessage] = useState('');
+  const [gameStatus, setGameStatus] = useState(game.status || 'lobby');
+  const [currentRound, setCurrentRound] = useState('');
+  const [enemies, setEnemies] = useState([]);
+  const [leader, setLeader] = useState('');
 
-
-
-  // Función para obtener los jugadores actualizados desde la API
   const fetchGameState = async () => {
-    const headers:any = {
-      'player': game.owner,
+    const headers: any = {
+      'player': playerName,
     };
     if (game.password) {
       headers.password = password;
     }
-
     try {
       const response = await fetch(`${SERVER}api/games/${game.id}`, {
         method: 'GET',
@@ -31,24 +31,131 @@ const GameScreen = ({ game, password, playerName }: any) => {
       });
       const data = await response.json();
       if (response.ok) {
-        setPlayers(data.data.players); // Actualizar los jugadores
+        setPlayers(data.data.players);
+        setGameStatus(data.data.status);
+        setCurrentRound(data.data.currentRound);
+        setEnemies(data.data.enemies);
+        if (gameStatus === 'rounds') { fetchRoundInfo() }
       } else {
-        console.error("Error al obtener el estado del juego:", response.status);
+        showModalWithMessage(`Error: ${response.status}`);
       }
     } catch (error) {
-      console.error("Error en la solicitud GET:", error);
+      throw new Error(`Error en la solicitud GET: ${error.message}`);
+    }
+  };
+
+  const fetchRoundInfo = async () => {
+    const headers: any = {
+      'player': playerName,
+    };
+    if (game.password) {
+      headers.password = password;
+    }
+    try {
+      const response = await fetch(`${SERVER}api/games/${game.id}/rounds/${currentRound}`, {
+        method: 'GET',
+        headers,
+      });
+      const data = await response.json();
+      if (response.ok) {
+        setLeader(data.data.leader);
+      } else {
+        showModalWithMessage(`Error al obtener la ronda: ${response.status}`);
+      }
+    } catch (error) {
+      throw new Error(`Error en la solicitud GET de ronda: ${error.message}`);
+    }
+  };
+
+  const selectGroup = (player) => {
+    setSelectedGroup((prevGroup) => {
+      if (prevGroup.includes(player)) {
+        return prevGroup.filter((p) => p !== player); // Deseleccionar
+      } else {
+        return [...prevGroup, player]; // Seleccionar
+      }
+    });
+  };
+
+
+  const submitGroup = async () => {
+    const headers: any = {
+      'player': playerName,
+      'Content-Type': 'application/json',
+    };
+    if (game.password) {
+      headers.password = password;
+    }
+
+    const body = JSON.stringify({ group: selectedGroup });
+
+    try {
+      const response = await fetch(`${SERVER}api/games/${game.id}/rounds/${currentRound}`, {
+        method: 'PATCH',
+        headers,
+        body,
+      });
+
+      const errorMsg = response.headers.get('X-msg');
+      if (response.ok) {
+        showModalWithMessage('Grupo enviado exitosamente.');
+      } else {
+        const statusMessages: any = {
+          401: `Credenciales inválidas: ${errorMsg || 'Sin mensaje'}`,
+          403: 'Acceso prohibido: solo el líder puede enviar el grupo',
+          404: `Ronda no encontrada: ${errorMsg || 'Sin mensaje'}`,
+        };
+        showModalWithMessage(statusMessages[response.status] || `Error desconocido: ${response.status}`);
+      }
+    } catch (error) {
+      setError(`Error al enviar el grupo: ${error.message}`);
+      console.error('Error al enviar el grupo:', error);
     }
   };
 
   useEffect(() => {
-    const intervalId = setInterval(fetchGameState, 5000);
+    const intervalId = setInterval(fetchGameState, 3000);
+    if (gameStatus === 'rounds') {
+      clearInterval(intervalId); // Detener el polling cuando el estado sea 'rounds'
+      fetchGameState();
+    }
     return () => clearInterval(intervalId);
-  }, [game.id, password, playerName]);
+  }, [gameStatus]);
 
-  const toggleStatus = () => {
-    setPlayerStatus((prevStatus) =>
-      prevStatus === 'ciudadano ejemplar' ? 'psicópata' : 'ciudadano ejemplar'
-    );
+  const isOwner = () => playerName.toLowerCase() === game.owner.toLowerCase();
+  const isEnemy = (player) => enemies.includes(player);
+  const imLeader = () => playerName === leader;
+
+  const startGame = async () => {
+    const data = {
+      player: game.owner,
+    };
+    if (game.password) {
+      data.password = password;
+    }
+    try {
+      const response = await fetch(`${SERVER}api/games/${game.id}/start`, {
+        method: 'HEAD',
+        headers: data,
+      });
+      const errorMsg = response.headers.get('X-msg');
+      if (response.status === 200) {
+        showModalWithMessage('El juego ha comenzado exitosamente.');
+        fetchGameState();
+      } else {
+        const statusMessages: any = {
+          401: `No autorizado: ${errorMsg || 'Sin mensaje'}`,
+          403: 'Acceso prohibido: usted no es un owner',
+          404: `Juego no encontrado: ${errorMsg || 'Sin mensaje'}`,
+          409: `El juego ya ha comenzado: ${errorMsg || 'Sin mensaje'}`,
+          428: `Se necesitan 5 jugadores para comenzar: ${errorMsg || 'Sin mensaje'}`,
+        };
+        showModalWithMessage(statusMessages[response.status] || `Error desconocido: ${response.status}`);
+      }
+    } catch (error) {
+      setError(`Error al iniciar el juego: ${error.message}`);
+      console.error('Error al iniciar el juego:', error);
+    }
   };
 
   const showModalWithMessage = (message: string) => {
@@ -60,89 +167,58 @@ const GameScreen = ({ game, password, playerName }: any) => {
     setShowModal(false);
   };
 
-
-  const isOwner = () => {
-    return playerName.toLowerCase() === game.owner.toLowerCase() || game.owner.toLowerCase().includes(playerName.toLowerCase());
-  };
-
-  // Función para iniciar el juego
-  const startGame = async () => {
-    const data={
-      player:game.owner
-    };
-    if (game.password) {
-      data.password = password;
-    }
-    try {
-      const response = await fetch(`${SERVER}api/games/${game.id}/start`, {
-        method: 'HEAD',
-        headers: data,
-      });
-
-      // Obtener el valor del encabezado "X-msg" si existe
-      const errorMsg = response.headers.get('X-msg');
-      // Manejar diferentes códigos de estado
-      if (response.status === 200) {
-        showModalWithMessage('El juego ha comenzado exitosamente.');
-      } else if (response.status === 401) {
-        showModalWithMessage(`No autorizado: ${errorMsg || 'Sin mensaje'}`);
-      } else if (response.status === 403) {
-        showModalWithMessage(`Acceso prohibido: usted no es un owner`);
-      } else if (response.status === 404) {
-        showModalWithMessage(`Juego no encontrado: ${errorMsg || 'Sin mensaje'}`);
-      } else if (response.status === 409) {
-        showModalWithMessage(`El juego ya ha comenzado: ${errorMsg || 'Sin mensaje'}`);
-      } else if (response.status === 428) {
-        showModalWithMessage(`Se necesitan 5 jugadores para comenzar: ${errorMsg || 'Sin mensaje'}`);
-      } else {
-        showModalWithMessage(`Error desconocido: ${response.status}`);
-      }
-
-    } catch (error) {
-      setError(`Error al iniciar el juego: ${error.message}`);
-      console.error('Error al iniciar el juego:', error);
-    }
-  };
-
   return (
     <div>
       <h1>Pantalla del Juego: {game.id}</h1>
       <h1>NOMBRE DEL JUGADOR: {playerName}</h1>
       <h1>Password: {password}</h1>
-
       <>
         {error && <Alert variant="danger">{error}</Alert>}
         <div className="game-interface">
           <div className="player-info">
-            <img
-              src="https://upload.wikimedia.org/wikipedia/en/7/73/Trollface.png"
-              alt="Foto del Jugador"
-              className="player-photo"
-            />
             <p>{playerName}</p>
-            <p
-              className={`player-status ${playerStatus === 'ciudadano ejemplar' ? 'status-good' : 'status-bad'}`}
-            >
-              {playerStatus}
-            </p>
+            {isEnemy(playerName) && <p className="enemy-marker"> Eres un enemigo </p>}
+            {imLeader() && <p className="leader-marker"> Eres el líder </p>}
           </div>
           <div className="players-div">
-            {/* Renderizar jugadores en círculos */}
             {players.map((player, index) => (
-              <div key={index} className="player-circle">
+              <button
+              key={index}
+              className={`player-button 
+                ${selectedGroup.includes(player) ? 'selected' : ''}
+                ${isEnemy(playerName) && isEnemy(player) ? 'enemy' : ''}
+              `}
+                onClick={() => imLeader() && selectGroup(player)}
+              >
                 <p>{player}</p>
-              </div>
+              </button>
             ))}
           </div>
-          <div className="actions">
-            <button>Trabajar</button>
-            <button>Sabotear</button>
-            <button onClick={toggleStatus}>Cambiar Estado</button>
-            {isOwner() && (
-              <button onClick={startGame}>Iniciar Juego</button>
-            )}          </div>
-        </div>
+          {imLeader() &&  (
+            <>
+              <div className="selected-group-info">
+                <h2>Grupo Seleccionado:</h2>
+                <ul>
+                  {selectedGroup.map((player, index) => (
+                    <li key={index}>{player}</li>
+                  ))}
+                </ul>
+              </div>
+            </>
+          )}
 
+
+          <div className="actions">
+            {isOwner() && gameStatus === 'lobby' && (
+              <button onClick={startGame}>Iniciar Juego</button>
+            )}
+            {imLeader() && gameStatus === 'rounds' && (
+              <>
+                <button onClick={submitGroup}>Enviar Grupo</button>
+              </>
+            )}
+          </div>
+        </div>
         <ModalComponent
           showModal={showModal}
           handleCloseModal={handleCloseModal}
