9a95e79d1346321be34595d265d6a221db84ff08
Brandon <brandonvargass12345@gmail.com>
2024-11-08 02:12:52 -0600

    urls

--- uiredes/app/components/NameCard.tsx
+++ uiredes/app/components/NameCard.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { Card, Button, Form, Alert } from 'react-bootstrap';
 
 const NameCard = ({ playerName, setPlayerName, setStage, setSERVER }: any) => {
-  const [server, setServer] = useState('http://localhost:5140/');
+  const [server, setServer] = useState('https://contaminados.akamai.meseguercr.com/');
   const [nameError, setNameError] = useState('');
 
   const handleServerChange = (e: any) => {
